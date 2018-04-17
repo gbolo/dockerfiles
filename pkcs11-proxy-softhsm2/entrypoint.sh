7bf4fdfa1b15e1eef09fe8b2842e3575ccf43eb5
gbolo <george.bolo@gmail.com>
2018-04-17 14:47:32 -0400

    adding support to create multiple slots

--- pkcs11-proxy-softhsm2/entrypoint.sh
+++ pkcs11-proxy-softhsm2/entrypoint.sh
@@ -14,13 +14,15 @@ echo "> pkcs11-proxy environment variables:"
 env | grep PKCS11_
 
 # hsm-pkcs11proxy --------------------------------------------------------------
-if ! $(softhsm2-util --show-slots | grep -ilq ".*Label.*${PKCS11_SLOT_LABEL}"); then
-  echo "> Initializing softhsm2 slot: ${PKCS11_SLOT_LABEL}"
-  softhsm2-util --init-token --free \
-    --label ${PKCS11_SLOT_LABEL} \
-    --pin ${PKCS11_SLOT_PIN} \
-    --so-pin ${PKCS11_SO_PIN}
-fi
+for PKCS11_SLOT_LABEL in $(echo ${PKCS11_SLOT_LABELS} | sed "s/,/ /g"); do
+  if ! $(softhsm2-util --show-slots | grep -ilq ".*Label.*${PKCS11_SLOT_LABEL}"); then
+    echo "> Initializing softhsm2 slot: ${PKCS11_SLOT_LABEL}"
+    softhsm2-util --init-token --free \
+      --label ${PKCS11_SLOT_LABEL} \
+      --pin ${PKCS11_SLOT_PIN} \
+      --so-pin ${PKCS11_SO_PIN}
+  fi
+done
 
 echo "> Executing as uid [$(/usr/bin/id -u)]: ${@}"
 exec "$@"
