184de03b1f1f2e78bed447609ce61047af516c37
gbolo <george.bolo@gmail.com>
2023-01-04 12:45:21 -0500

    trapping signals for clean proxy shutdown

--- wireguard-proxy/entrypoint.sh
+++ wireguard-proxy/entrypoint.sh
@@ -5,23 +5,39 @@
 #    for use with image: gbolo/wireguard-proxy
 #
 
-launch_all_wg_interfaces() {
+get_all_wg_interfaces() {
     wg_interfaces=`find /etc/wireguard -type f -name "*.conf"`
     if [ -z $wg_interfaces ]; then
         echo "could not find any *.conf files in /etc/wireguard. Exiting..." >&2
         exit 1
     fi
+}
 
+launch_all_wg_interfaces() {
+    get_all_wg_interfaces
     for interface in $wg_interfaces; do
         echo "[$(date)] wg-quick up $interface"
         wg-quick up $interface
     done
 }
 
+terminate_all() {
+    get_all_wg_interfaces
+    for interface in $wg_interfaces; do
+        echo "[$(date)] wg-quick down $interface"
+        wg-quick down $interface
+    done
+    echo "[$(date)] killall gost"
+    killall gost
+}
+
 launch_proxy() {
-    echo "[$(date)] starting socks+http proxy"
-    /usr/bin/gost -L :33080
+    echo "[$(date)] starting socks+http proxy (gost)"
+    gost -L :33080 &
 }
 
 launch_all_wg_interfaces
 launch_proxy
+
+trap "terminate_all" SIGINT SIGTERM SIGQUIT
+wait
